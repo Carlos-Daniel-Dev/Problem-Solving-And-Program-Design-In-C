a7b0694e91d13806990a64a009bf8b054237b183
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-03-09 11:50:17 -0300

    now memory is a constant value

--- section-1.6/my_floating_numbers.c
+++ section-1.6/my_floating_numbers.c
@@ -8,16 +8,19 @@
 #define CHAR 1
 #define FLOAT 2
 
-void print_memory(char *memory) 
+char memory[MEMORY_SPACE];
+
+void print_memory() 
 {
     for (int i = 0; i < MEMORY_SPACE; i++) 
     {
         printf("%c", memory[i]);
     }
+
     printf("\n");
 }
 
-void set_byte_on_memory(char *memory, int pos, char *byte) 
+void set_byte_on_memory(int pos, char *byte) 
 {
     int index = pos * 8;
     for (int i = 0; i < 8; i++) 
@@ -37,7 +40,7 @@ unsigned char binaryToByte(const char *binaryString) {
     return byte;
 }
 
-unsigned char get_byte_on_memory(char *memory, int pos)
+unsigned char get_byte_on_memory(int pos)
 {
     int index = pos * 8;
     char byte[8];
@@ -51,14 +54,11 @@ unsigned char get_byte_on_memory(char *memory, int pos)
 
 int main() 
 {
-    char *memory = (char *) malloc(MEMORY_SPACE * sizeof(char));
-    set_byte_on_memory(memory, 0, "01010101");
+    set_byte_on_memory(0, "1111");
     
-    unsigned char byte = get_byte_on_memory(memory, 0);
+    unsigned char byte = get_byte_on_memory(0);
     int num = (int)byte;
     
     printf("%d\n", num);
-    
-    free(memory);
     return 0;
 }
