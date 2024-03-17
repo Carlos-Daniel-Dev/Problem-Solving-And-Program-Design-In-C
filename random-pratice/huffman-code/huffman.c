c1472f1fc74deb7a0c40e054cac6f121839f4ca5
CarlosDaniel <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-03-17 10:14:41 -0300

    build_huffman_tree;  find_min_frequencies; print_huffman_tree

--- random-pratice/huffman-code/huffman.c
+++ random-pratice/huffman-code/huffman.c
@@ -146,7 +146,82 @@ void free_huffman_tree(Node *root)
     free(root);
 }
 
+void find_min_frequencies(Node **nodes, int size, int *min1, int *min2) 
+{
+    *min1 = -1;
+    *min2 = -1;
 
+    for (int i = 0; i < size; i++) {
+        if (nodes[i] != NULL) {
+            if (*min1 == -1 || nodes[i]->frequency < nodes[*min1]->frequency) {
+                *min2 = *min1;
+                *min1 = i;
+            } else if (*min2 == -1 || nodes[i]->frequency < nodes[*min2]->frequency) {
+                *min2 = i;
+            }
+        }
+    }
+}
+
+Node *build_huffman_tree(int **frequency_list) {
+    // Initialize a list of pointers to nodes
+    Node **nodes = (Node **)malloc(ASCII_CHARACTERS * sizeof(Node *));
+    if (nodes == NULL) {
+        fprintf(stderr, "Error allocating memory for node list.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize leaf nodes with frequencies and corresponding characters
+    for (int i = 0; i < ASCII_CHARACTERS; i++) {
+        if (frequency_list[i][1] > 0) {
+            nodes[i] = create_node(frequency_list[i][1], (unsigned char)frequency_list[i][0]);
+        } else {
+            nodes[i] = NULL; // This character doesn't exist in the text
+        }
+    }
+
+    // Build the Huffman tree
+    while (1) {
+        // Find the two nodes with the lowest frequencies
+        int min1, min2;
+        find_min_frequencies(nodes, ASCII_CHARACTERS, &min1, &min2);
+
+        // If only one node remains, it will be the root of the tree
+        if (min2 == -1) break;
+
+        // Create a new node with the sum of the frequencies of the two lowest nodes
+        Node *new_node = create_node(nodes[min1]->frequency + nodes[min2]->frequency, '\0');
+        new_node->left = nodes[min1];
+        new_node->right = nodes[min2];
+
+        // Mark used nodes as NULL
+        nodes[min1] = NULL;
+        nodes[min2] = new_node;
+    }
+
+    // Find and return the root of the tree
+    Node *root = NULL;
+    for (int i = 0; i < ASCII_CHARACTERS; i++) {
+        if (nodes[i] != NULL) {
+            root = nodes[i];
+            break;
+        }
+    }
+
+    // Free memory of the node list
+    free(nodes);
+
+    return root;
+}
+
+void print_huffman_tree(Node *root) {
+    if (root == NULL) return;
+    print_huffman_tree(root->left);
+    if (root->character != '\0') {
+        printf("[%c | %d]\n", root->character, root->frequency);
+    }
+    print_huffman_tree(root->right);
+}
 
 int main() 
 {
