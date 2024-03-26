f0a3ddaa6ca9009d9c956c3e8ebe5cb1be16578f
carlosdaniel26 <105173667+Carlos-Daniel-Dev@users.noreply.github.com>
2024-03-26 17:29:40 -0300

    mathematical expressions in c functions

---
+++ section-3/3_mathematical_expressions.c
@@ -0,0 +1,39 @@
+/*
+* Programmer: Carlos completed: March 14, 2024
+*
+* Instructor: Book
+*
+* Description: Rewrite mathematical expressions using functions
+* Parameters to compile: -lm
+* 
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+double function_a(double u, double v, double w)
+{
+	return sqrt(u + (v * (w * w)));
+}
+
+double function_b(double x, double y)
+{
+	return log(pow(x, y));
+}
+
+double function_c(double x, double y)
+{
+	return sqrt(pow((x - y), 3));
+}
+
+double function_d(double x, double y, double w, double z)
+{
+	return (x * y) - (w / z);
+}
+
+
+int main ()
+{	
+
+	return 1;
+}
